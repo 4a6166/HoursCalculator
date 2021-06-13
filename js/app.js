14c96df5bb7e4cbfb021726a9642b8876ed9b993
Jacob Field <48722956+Prelimine-JF@users.noreply.github.com>
2021-06-13 23:53:02 +0200

    commit before changing second label

--- js/app.js
+++ js/app.js
@@ -1,5 +1,104 @@
 console.log('Hours Calculator loaded');
 
+//#region Utilities
+function inputTrim(input, max){
+    if (input.value < 0) alert(`Please enter a value between 0 and ${max}`);
+
+    input.value = Math.abs(input.value) > 0 ? Math.abs(input.value) : null;
+
+    input.value = input.value < Number(max) ? input.value : null;
+
+}
+
+function switchMonthNumToString(number){
+    switch (Number(number)) {
+        case 1:
+            return "January";
+            break;
+        case 2:
+            return "February";
+            break;
+        case 3:
+            return "March";
+            break;
+        case 4:
+            return "April";
+            break;
+        case 5:
+            return "May";
+            break;
+        case 6:
+            return "June";
+            break;
+        case 7:
+            return "July";
+            break;
+        case 8:
+            return "August";
+            break;
+        case 9:
+            return "September";
+            break;
+        case 10:
+            return "October";
+            break;
+        case 11:
+            return "November";
+            break;
+        case 12:
+            return "December";
+            break;
+    }
+}
+
+function switchMonthStringToNum(string){
+    switch (string) {
+        case "January":
+            return 1;
+            break;
+        case "February":
+            return 2;
+            break;
+        case "March":
+            return 3;
+            break;
+        case "April":
+            return 4;
+            break;
+        case "May":
+            return 5;
+            break;
+        case "June":
+            return 6;
+            break;
+        case "July":
+            return 7;
+            break;
+        case "August":
+            return 8;
+            break;
+        case "September":
+            return 9;
+            break;
+        case "October":
+            return 10;
+            break;
+        case "November":
+            return 11;
+            break;
+        case "December":
+            return 12;
+            break;
+    }
+}
+
+//#endregion
+
+//#region Vars
+let date = new Date();
+let currentMonth = date.getMonth() + 1;
+
+
 let data = {
     hoursRequirement: undefined,
     hoursAllowableProBono: undefined,
@@ -18,343 +117,214 @@ let calcs = {
     hrsLeft_PerMonth: undefined,
 };
 
-const hoursRequirement = document.getElementById('BillHrReq');
-hoursRequirement.addEventListener('change', _updateData);
+const input_hoursRequirement = document.getElementById('BillHrReq');
+input_hoursRequirement.addEventListener('change', handleChange);
 
-const hoursAllowableProBono = document.getElementById('PBHrs');
-hoursAllowableProBono.addEventListener('change', _updateData);
+const input_hoursAllowableProBono = document.getElementById('PBHrs');
+input_hoursAllowableProBono.addEventListener('change', handleChange);
 
-const billableYearMonthPicker = document.getElementById('billableYearMonth');
-billableYearMonthPicker.addEventListener('change', _updateData);
-
+const input_billableYearMonthPicker = document.getElementById('billableYearMonth');
+input_billableYearMonthPicker.addEventListener('change', handleChange);
 
 const table = document.getElementById('hrsTable');
 const tableRows = table.children[1].children;
-table.addEventListener('change', _updateData);
+table.addEventListener('change', handleChange);
 
+const output_monthsRemaining = document.getElementById('monthsLeft');
+const output_hoursRemaining = document.getElementById('hrsLeftBillable');
+const output_hrsLeftProBono = document.getElementById('hrsLeftProBono');
+const output_hrsLeftPerMonth = document.getElementById('hrsLeftPerMonth');
+const output_hrsBig = document.getElementById('hrs');
 
-const monthsRemaining1 = document.getElementById('monthsLeft');
-const hoursRemaining = document.getElementById('hrsLeftBillable');
-const hrsLeftProBono = document.getElementById('hrsLeftProBono');
-const hrsLeftPerMonth = document.getElementById('hrsLeftPerMonth');
+//#endregion
 
-function _updateData(e){
-    console.log("setting local storage");
+function handleChange(e){
+    console.log("Setting Local Storage");
     updateData();
+    updateCalcs();
+    updateOutput();
     localStorage.setItem("hrsCalculator", JSON.stringify(data));
+    updateTableMonths();
 }
 
+//#region Update and storage
 function updateData(){
-    data.hoursRequirement = hoursRequirement.value;
-    data.hoursAllowableProBono = hoursAllowableProBono.value;
-    data.startMonth = billableYearMonthPicker.value;
+    data.hoursRequirement = Number(input_hoursRequirement.value);
+    data.hoursAllowableProBono = Number(input_hoursAllowableProBono.value);
+    data.startMonth = Number(input_billableYearMonthPicker.value);
 
     data.hours.billable = [];
     data.hours.proBono =    [];
     for (let i = 0; i<12; i++){
-        console.log("billable: "+tableRows[i].children[1].children[0].value)
-        data.hours.billable.push(tableRows[i].children[1].children[0].value);
-        data.hours.proBono.push(tableRows[i].children[1].children[0].value);
+        // console.log("billable: "+tableRows[i].children[1].children[0].value)
+        data.hours.billable.push(Number(tableRows[i].children[1].children[0].value));
+        data.hours.proBono.push(Number(tableRows[i].children[2].children[0].value));
     }
 }
 
+function updateOutput() {
+    output_monthsRemaining.textContent = calcs.monthsRemaining;
+    output_hoursRemaining.textContent = calcs.hrsLeft_Total;
+    output_hrsLeftProBono.textContent = calcs.hrsLeft_ProBono;
+    output_hrsLeftPerMonth.textContent = calcs.hrsLeft_PerMonth;
+    output_hrsBig.textContent = calcs.hrsLeft_PerMonth;
+}
+
 (function onLoad(){
+    setUpTable();
+
+    console.log("Getting from Local Storage")
     if(localStorage.getItem("hrsCalculator")){
-        console.log(localStorage.getItem("hrsCalculator"));
         data = JSON.parse(localStorage.getItem("hrsCalculator"));
         
-        hoursRequirement.value = data.hoursRequirement;
-        hoursAllowableProBono.value = data.hoursAllowableProBono;
-        billableYearMonthPicker.value = data.startMonth;
+        input_hoursRequirement.value = data.hoursRequirement;
+        input_hoursAllowableProBono.value = data.hoursAllowableProBono;
+        input_billableYearMonthPicker.value = data.startMonth;
 
-        for( let i = 0; i < tableRows.length; i++){
-            tableRows[i].children[1].children[0].value = data.hours.billable[i];
-            tableRows[i].children[2].children[0].value = data.hours.proBono[i];            
-        }
-
-        monthsRemaining1.value = calcs.monthsRemaining;
-        hoursRemaining.value = calcs.hrsLeft_Total;
-        hrsLeftProBono.value = calcs.hrsLeft_ProBono;
-        hrsLeftPerMonth.value = calcs.hrsLeft_PerMonth
-    }
+        updateTableMonths();
+        setTableHours();
+        updateCalcs();
+        updateOutput();
+    } else console.log("Hours Calculator local information not found");
 })();
 
+function setUpTable(){
+
+    let rowTemplate = `
+        <tr>
+            <td>{{Month}}</td>
+            <td style="padding: 0;">
+                <input class="billable" type="number" min="0" max="730" oninput="inputTrim(this, 730)" placeholder="0">
+            </td>
+            <td style="padding: 0;">
+                <input class="probono" type="number" min="0" max="730" oninput="inputTrim(this, 703)" placeholder="0">
+            </td>
+        </tr>
+        `
+
+    for (let i = 0; i<12; i++){
+        table.children[1].innerHTML += rowTemplate;
+    }
+}
+
+function setTableHours() {
+    for (let i = 0; i < tableRows.length; i++) {
+        if (data.hours.billable[i]){
+            tableRows[i].children[1].children[0].value = data.hours.billable[i];
+        } else tableRows[i].children[1].children[0].value = null;
+        if (data.hours.proBono[i]){
+            tableRows[i].children[2].children[0].value = data.hours.proBono[i];
+        } else tableRows[i].children[2].children[0].value = null;
+    }
+}
+
+function updateTableMonths() {
+    let month = data.startMonth;
+
+    for (let i = 0; i < tableRows.length; i++){
+        let row = tableRows[i].children[0];
+
+        let num = Number(month) +i;
+        if (num > 12) {
+            num -= 12;
+        }
+        // row.innerHTML = "<td>"+(switchMonthNumToString(num))+"</td>";
+        row.textContent = switchMonthNumToString(num);
+    }
+}
+
+function resetTableHours(){
+    data.hours.billable = [];
+    data.hours.proBono = [];
+
+    updateCalcs();
+    updateOutput();
+    setTableHours();
+    localStorage.setItem("hrsCalculator", JSON.stringify(data));
+}
+
+//#endregion
+
+//#region Calculate
+
+function monthsRemaining(startMonth)
+{
+    let result = 0;
+
+    if (startMonth === currentMonth){
+        result = 12;
+    } else if(startMonth > currentMonth) {
+        result = Number(startMonth - currentMonth);
+    } else {
+        result = 12 - Number(currentMonth - startMonth)
+    }
+
+    return result;
+}
+
+function updateCalcs(){
+    calcs.monthsRemaining= Math.ceil(monthsRemaining(data.startMonth));
+
+    let proBonoHrsTotal = 0;
+    let billHrsTotal = 0;
+    let billHrsLeft = 0;
+
+    for (i = 0; i<data.hours.proBono.length; i++){
+        if(data.hours.proBono[i]){
+            proBonoHrsTotal += data.hours.proBono[i];
+        }
+    }
+
+    calcs.hrsLeft_ProBono =  proBonoHrsTotal > data.hoursAllowableProBono ? 0 : Math.ceil(data.hoursAllowableProBono - proBonoHrsTotal);
+
+    for (i = 0; i<data.hours.billable.length; i++){
+        if(data.hours.billable[i]){
+            billHrsTotal += data.hours.billable[i];
+        }
+    }
+
+    proBonoHrsUsed = data.hoursAllowableProBono - calcs.hrsLeft_ProBono;
+    billHrsLeft = data.hoursRequirement - billHrsTotal - proBonoHrsUsed;
+
+    calcs.hrsLeft_Total = billHrsLeft > 0 ? Math.ceil(billHrsLeft) : 0;
+    calcs.hrsLeft_PerMonth = Math.ceil(calcs.hrsLeft_Total/calcs.monthsRemaining);
 
 
+    data_donut_hours = [];
+    for (let i =0; i< data.hours.billable.length; i++){
+        if(i<(12-calcs.monthsRemaining) || (data.hours.billable[i] >0)){
+            data_donut_hours.push(data.hours.billable[i]);
+        } else {
+            data_donut_hours.push(calcs.hrsLeft_PerMonth);
+        }
+    }
 
+    data_donut_probono = [proBonoHrsUsed, calcs.hrsLeft_ProBono];
 
-// //TODO: Fill data and calcs objects, load data from local storage on startup, save on update, have functions fill calcs and have display pull from it
+    updateDonut();
+    // makeDonut(data.hours.billable, data_donut_probono);
+}
 
-// const picker = $("#billableYearMonth");
+function updateDonut(){ 
 
-// const annualBillReq = document.querySelector('#BillHrReq');
-// annualBillReq.addEventListener('change', updateTableEvent);
+    for (let i= 0; i<12; i++){
+        let num = Number(data.startMonth) +i;
+        if (num > 12) {
+            num -= 12;
+        }
 
-// const otherHrs = document.querySelector('#PBHrs');
-// otherHrs.addEventListener('change', updateTableEvent);
+        donut.data.labels[i] = switchMonthNumToString(num);
 
-// function updateTableEvent(e) { 
-//     // updateTable();
-//     // setLocalStorage();
-// }
-
-// function setLocalStorage(){
-//     let billableYearMonth = $("#billableYearMonth").find(":selected").val();
-//     localStorage.setItem("billableYearMonth", billableYearMonth);
-
-//     let annualBillReq = $("#BillHrReq").val();
-//     localStorage.setItem("annualBillReq", annualBillReq);
-
-//     let otherHrs = $("#PBHrs").val();
-//     localStorage.setItem("otherHrs", otherHrs);
-
-//     // TODO: get/set local storage for all items in the table
-// }
-
-// function getFromLocalStorage(){
-//     let annualBillReq = Number(localStorage.getItem("annualBillReq"));
-//     let annualBillReqInput = $("#BillHrReq");
-//     if(annualBillReq > 0){
-//         annualBillReqInput.val(annualBillReq);
-//     } else annualBillReqInput.val(undefined)
-
-//     let otherHrs = Number(localStorage.getItem("otherHrs"));
-//     let otherHrsInput = $("#PBHrs");
-//     if(otherHrs > 0){
-//         otherHrsInput.val(otherHrs);
-//     } else otherHrsInput.val(undefined);
-
-//     let localMonthNum = localStorage.getItem("billableYearMonth");
-//     let localMonth = switchMonthNumToString(Number(localMonthNum));
-//     let sel = picker.find(`:contains(${localMonth})`);
-//     sel.prop("selected", true);
-// }
-
-// function updateTable() {
-//     let table = $('#hrsTable tbody tr');
-//     // console.log(table);
-    
-//     let month = $("#billableYearMonth").find(":selected").val();
-//     // console.log(month);
-
-//     for (let i = 0; i < table.length; i++){
-//         let row = table[i].children[0];
-
-//         let num = Number(month) +i;
-//         if (num > 12) {
-//             num -= 12;
-//         }
-//         // console.log(switchMonthNumToString(num));
-//         row.innerHTML = "<td>"+(switchMonthNumToString(num))+"</td>";
+        if(i>=(12-calcs.monthsRemaining)){
+            donut.data.datasets[0].backgroundColor[i] = CHART_COLORS.prelimine_Gray;
+        } else donut.data.datasets[0].backgroundColor[i] = CHART_COLORS.prelimine_Orange;
         
-//     }
-// }
-
-
-// function switchMonthNumToString(number){
-//     switch (number) {
-//         case 1:
-//             return "January";
-//             break;
-//         case 2:
-//             return "February";
-//             break;
-//         case 3:
-//             return "March";
-//             break;
-//         case 4:
-//             return "April";
-//             break;
-//         case 5:
-//             return "May";
-//             break;
-//         case 6:
-//             return "June";
-//             break;
-//         case 7:
-//             return "July";
-//             break;
-//         case 8:
-//             return "August";
-//             break;
-//         case 9:
-//             return "September";
-//             break;
-//         case 10:
-//             return "October";
-//             break;
-//         case 11:
-//             return "November";
-//             break;
-//         case 12:
-//             return "December";
-//             break;
-//     }
-// }
-
-// function switchMonthStringToNum(string){
-//     switch (string) {
-//         case "January":
-//             return 1;
-//             break;
-//         case "February":
-//             return 2;
-//             break;
-//         case "March":
-//             return 3;
-//             break;
-//         case "April":
-//             return 4;
-//             break;
-//         case "May":
-//             return 5;
-//             break;
-//         case "June":
-//             return 6;
-//             break;
-//         case "July":
-//             return 7;
-//             break;
-//         case "August":
-//             return 8;
-//             break;
-//         case "September":
-//             return 9;
-//             break;
-//         case "October":
-//             return 10;
-//             break;
-//         case "November":
-//             return 11;
-//             break;
-//         case "December":
-//             return 12;
-//             break;
-//     }
-// }
-
-
-// function setYear(){
-//     let y = new Date().getFullYear().toString();
-//     picker.find(":selected").prop("selected", false)
-// }
-
-// function monthsRemaining(startMonth, currentMonth)
-// {
-//     let result = 0;
-
-//     if (startMonth === currentMonth){
-//         result = 12;
-//     } else if(startMonth > currentMonth) {
-//         result = Number(startMonth - currentMonth);
-//     } else {
-//         result = 12 - Number(currentMonth - startMonth)
-//     }
-
-//     console.log("Months Remaining: "+result);
-//     return result;
-// }
-
-// function getHours(className) {
-//     let result = 0;
-//     let cells = document.getElementsByClassName(className);
-
-//     for(let i = 0; i<cells.length; i++) {
-//         result += Number(cells[i].value);
-//     }
-//     console.log("Months with "+className+" hours: "+cells.length);
-//     let remaining ="";
-//     for (let i = 1; i < className.length; i++){
-//         remaining += className[i];
-//     }
-//     console.log(className[0].toUpperCase() + remaining +" Hours Found: "+result);
-//     return result;
-// }
-
-// function calcHours() {    
-//     // get billable hour requirement
-//     let billableHoursReq = $("#BillHrReq").val();
-//     // get pro bono and other non-billable hours counting towards billable max
-//     let proBonoHoursMax = $("#PBHrs").val();
-//     // get month that starts the billable year
-//     // let startMonth = Number($("#billableYearMonth").val().split("-")[1]);
-//     let startMonth = $("#billableYearMonth").find(":selected").val();
-//     console.log("Billable Year Start Month: "+startMonth);
-
-//     //get array of billable hours for each month
-//     let billableHrsSum = getHours("billable");
-//     let hrsLeftPerMonth = 0;
-//     //get array of PB hours for each month
-//     let proBonoHrsSum = getHours("probono");
-
-//     //result variables
-//     let billableHrsRemaining = 0;
-//     let proBonoHoursRemaining = (proBonoHrsSum <= proBonoHoursMax) ? (Number(proBonoHoursMax) - Number(proBonoHrsSum)) : 0;
-
-//     let date = new Date();
-//     let currentMonth = date.getMonth()+1;
-//     console.log("Current Month: " +currentMonth);
-//     let monthsLeft = monthsRemaining(startMonth, currentMonth)
-    
-
-//     if(billableHoursReq == 0){
-//         alert("Please enter the Annual Billable Hours Requirement.")
-//     } else { 
-//         if (proBonoHoursRemaining > 0){
-//             billableHrsRemaining = Number(billableHoursReq) - (Number(billableHrsSum) + Number(proBonoHrsSum));
-//         } else {
-//             billableHrsRemaining = Number(billableHoursReq) - (Number(billableHrsSum) + Number(proBonoHoursMax));
-//         }
-
-//         if (billableHrsRemaining >0){
-//             hrsLeftPerMonth = Math.ceil(billableHrsRemaining / monthsLeft);
-//         } else {
-//             hrsLeftPerMonth = 0;
-//         }
         
-//         console.log("Total Billable Hours Remaining: "+ billableHrsRemaining);
-//         console.log("Pro Bono/Other Hours Left Available: "+proBonoHoursRemaining);
-//         console.log("Hours Remaining Per Month: "+hrsLeftPerMonth);
+    }
 
-//         updateResults(monthsLeft, billableHrsRemaining, proBonoHoursRemaining, hrsLeftPerMonth);
-//     }
-// }
+    donut.data.datasets[0].data = data_donut_hours;
+    donut.data.datasets[1].data = data_donut_probono;
 
-// function updateResults(monthsLeft, hoursLeft, hoursLeftPB, hoursLeftMonth) {
-//     let months = $("#monthsLeft").text(monthsLeft);
-//     let hrs = $("#hrsLeftBillable").text(hoursLeft);
-//     let hrsPB = $("#hrsLeftProBono").text(hoursLeftPB);
-//     let hrsMonth = $("#hrsLeftPerMonth").text(hoursLeftMonth);
-// }
+    donut.update();
+}
 
-// function resetFields(){
-//     $("#BillHrReq").val(null);
-//     $("#PBHrs").val(null);
-//     setYear();
-
-
-//     let cellsBill = document.getElementsByClassName("billable");
-//     for (let i = 0; i<cellsBill.length; i++)
-//     {
-//         cellsBill[i].value = null;
-//     }
-
-//     let cellsPB = document.getElementsByClassName("probono");
-//     for (let i = 0; i<cellsPB.length; i++)
-//     {
-//         cellsPB[i].value = null;
-//     }
-
-//     updateResults(0, 0, 0, 0)
-//     updateTable();
-// }
-
-
-
-
-
-// (function (){
-//     setYear();
-//     getFromLocalStorage();
-//     updateTable();
-// })()
--- js/chart-config.js
+++ js/chart-config.js
@@ -11,159 +11,12 @@ const CHART_COLORS = {
     green: 'rgb(75, 192, 192)',
     blue: 'rgb(54, 162, 235)',
     purple: 'rgb(153, 102, 255)',
-    grey: 'rgb(201, 203, 207)'
+    grey: 'rgb(201, 203, 207)',
+    prelimine_Orange: 'rgb(221, 109, 79)',
+    prelimine_Blue: 'rgb(77, 130, 184)',
+    prelimine_Gray: 'rgb(128, 128, 128)',
 };
 
-
-/// Total Hours
-
-//Setup
-const DATA_COUNT = 1;
-const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 730};
-const labels = [
-'January',
-'February',
-'March',
-'April',
-'May',
-'June',
-'July',
-'August',
-'September',
-'October',
-'November',
-'December'
-];
-const _data = [120, 150, 210, 35, 110, 115, 75, 110, 95, 100, 100, 100];
-const data_totalHrs = {
-    labels: labels,
-    datasets: [{
-        label: 'Hours',
-        borderColor: 'rgb(255, 99, 132)',
-        borderWidth: .2,
-        backgroundColor: [
-            transparentize(CHART_COLORS.red, 0.5),
-            transparentize(CHART_COLORS.orange, 0.5),
-            transparentize(CHART_COLORS.yellow, 0.5),
-            transparentize(CHART_COLORS.green, 0.5),
-            transparentize(CHART_COLORS.blue, 0.5),
-            transparentize(CHART_COLORS.purple, 0.5),
-            transparentize(CHART_COLORS.red, 0.5),
-            transparentize(CHART_COLORS.orange, 0.5),
-            transparentize(CHART_COLORS.yellow, 0.5),
-            // transparentize(CHART_COLORS.green, 0.5),
-            // transparentize(CHART_COLORS.blue, 0.5),
-            // transparentize(CHART_COLORS.purple, 0.5),
-            transparentize(CHART_COLORS.grey, 0.2),
-            transparentize(CHART_COLORS.grey, 0.2),
-            transparentize(CHART_COLORS.grey, 0.2),
-        ],
-        data: _data,
-    }]
-};
-
-//Config
-const config_totalHrs = {
-    type: 'polarArea',
-    data: data_totalHrs,
-    options: {
-        responsive: true,
-        scales: {
-            r: {
-                grid: {
-                    color: CHART_COLORS.orange,
-                },
-                ticks: {
-                    color: CHART_COLORS.orange,
-                    // display: false,
-                    maxTicksLimit: 5,
-                    stepSize: 75,
-                }
-            }
-        },
-        plugins: {
-            legend: {
-                display: false,
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: 'Hours',
-                // position: 'bottom',
-            },
-        }
-    }
-};
-
-//render
-var totalHrs = new Chart(
-    document.getElementById('chart-totalHrs'),
-    config_totalHrs
-);
-
-/// Pro Bono stacked bar
-
-// setup
-const DATA_COUNT_totalProBono = 1;
-const NUMBER_CFG_totalProBono = {count: DATA_COUNT_totalProBono, min: -100, max: 100};
-
-const labels_totalProBono = ["Pro Bono Hours"]
-const _data_totalProBono_1 = [2]
-const _data_totalProBono_2 = [1]
-const data_totalProBono = {
-    labels: labels_totalProBono,
-    datasets: [
-        {
-            label: 'Hours Used',
-            data: _data_totalProBono_1,
-            backgroundColor: CHART_COLORS.red,
-        },
-        {
-            label: 'Hours Remaining',
-            data: _data_totalProBono_2,
-            backgroundColor: CHART_COLORS.blue,
-        }
-    ]
-};
-
-// config
-const config_totalProBono = {
-    type: 'bar',
-    data: data_totalProBono,
-    options: {
-        plugins: {
-            legend: {
-                display: false,
-            },
-            scaleShowLabels : false,
-            title: {
-                display: true,
-                text: 'Pro Bono Hours'
-            },
-        },
-        maintainAspectRatio: false,
-        responsive: true,
-        scales: {
-            x: {
-            stacked: true,
-            display: false,
-            },
-            y: {
-            stacked: true,
-            display: false,
-            }
-        }
-    }
-};
-
-//reder 
-
-var totalProBono = new Chart(
-    document.getElementById('chart-totalProBono'),
-    config_totalProBono
-);
-
-/// Donut
 
 //setup
 const DATA_COUNT_donut = 5;
@@ -173,19 +26,32 @@ let data_donut_hours = [2, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6 ,7];
 let data_donut_probono = [20, 10];
 
 const data_donut = {
-    labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+    labels: [],
     datasets: [
         {
             label: 'Hours',
             data: data_donut_hours,
-            backgroundColor: CHART_COLORS.orange,
+            backgroundColor: [
+                CHART_COLORS.prelimine_Orange,
+                CHART_COLORS.prelimine_Orange,
+                CHART_COLORS.prelimine_Orange,
+                CHART_COLORS.prelimine_Orange,
+                CHART_COLORS.prelimine_Orange,
+                CHART_COLORS.prelimine_Orange,
+                CHART_COLORS.prelimine_Orange,
+                CHART_COLORS.prelimine_Orange,
+                CHART_COLORS.prelimine_Orange,
+                CHART_COLORS.prelimine_Orange,
+                CHART_COLORS.prelimine_Orange,
+                CHART_COLORS.prelimine_Orange,
+            ]
         },
         {
             label: 'Pro Bono',
             data: data_donut_probono,
             backgroundColor: [
-                CHART_COLORS.orange,
-                CHART_COLORS.grey,
+                CHART_COLORS.prelimine_Orange,
+                CHART_COLORS.prelimine_Gray,
             ],
             hoverOffset: 4,
         }
@@ -199,6 +65,7 @@ const config_donut = {
     options: {
 
         responsive: true,
+        maintainAspectRatio: false,
         circumference: 180,
         rotation: 270,
         scales: {
@@ -220,7 +87,7 @@ const config_donut = {
 Chart.overrides.doughnut.cutout = 90;
 
 //render
-var donut =  new Chart(
-    document.getElementById('chart-dounut'),
-    config_donut
-)
+    var donut =  new Chart(
+        document.getElementById('chart-dounut'),
+        config_donut
+    )
